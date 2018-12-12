bc137d708cae9fd1fe042206e86adaf1bb0b666e
Wang Jinlong <jlwang@nullmax.ai>
2018-12-12 19:25:13 +0800

    hello concurrent world

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <thread>
+
+void hello(){
+  std::cout<<"Hello Concurrent World"<<std::endl;
+}
 
 int main(int argc, char* argv[]){
+  std::thread t(hello);
+  t.join();
 	std::cout<< "Hello world!"<<std::endl;
 	return 0;
 }
